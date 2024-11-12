f6036c2ca0b7a71534a3dbd7f0187b84691bd26d
mohamed louati <mohamedlouati002@gmail.com>
2024-11-12 09:44:20 +0100

    Initial commit

--- iotapp/mqtt_client.py
+++ iotapp/mqtt_client.py
@@ -1,7 +1,11 @@
 import paho.mqtt.client as mqtt
 import json
-from .models import cheepNecklace
+from pymongo import MongoClient
 
+mongo_uri ="mongodb+srv://mohamedlouati002:HDFMzGC3KTs56vJR@cluster0.62oql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client_mongo=MongoClient(mongo_uri )
+db=client_mongo["AHDProject0"]
+collection=db["SensorData"]
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -14,19 +18,13 @@ def on_message(client, userdata, msg):
     try:
         payload = msg.payload.decode()
         data = json.loads(payload)
-        print(f"recieved json: {data}")
+        print(f"Received JSON: {data}")
 
-        cheepNecklace.objects.create(
-            idNecklace=data["idMouton"],
-            acceleration=data["acc"],
-            gyroscope=data["gyr"],
-            pulse=data["pulse"],
-            temperature=data["temp"],
-            time=data["time"]
-        )
-        
+        # Insert the data into MongoDB collection
+        collection.insert_one(  data )
+
     except json.JSONDecodeError as e:
-        print(f"JSON failed {e}")
+        print(f"JSON Decode Error: {e}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
